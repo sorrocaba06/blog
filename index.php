f4a1251f37978c1de7191691e2a4caf53580960f
sorrocaba06 <luciusmuniz519@hotmail.com>
2019-08-16 17:18:31 -0300

    Mostrando Titulo

--- index.php
+++ index.php
@@ -8,22 +8,26 @@ $dbpass = "";
 try
 {
     $pdo = new PDO($dsn, $dbuser, $dbpass);
-    echo "Conexão estabelecida com sucesso";
+    echo "<h2>Conexão estabelecida com sucesso</h2>";
     
     $sql = "SELECT * FROM posts";
     $dado = $pdo->query($sql);
     
     if($dado->rowCount() > 0)
     {
-        echo "Há posts cadastrados";
+        echo "<h3>Há posts cadastrados</h3>";
+        foreach($dado->fetchAll() as $post)
+        {
+            echo "<p><b>Titulo</b>: ".$post['titulo']."<br>";
+        }
     }
     else
     {
-        echo "Não há posts cadastrados";
+        echo "<h3>Não há posts cadastrados</h3>";
     }
 }
 catch(PDOExecption $e)
 {
-    echo "Falho: ".$e->getMessage();
+    echo "<h2>Falho: ".$e->getMessage()."</h2>";
 }
 ?>
